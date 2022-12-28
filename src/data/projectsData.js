7a50ca7fbc113585ac34d0417c23f88b6b548643
Hidayat-Ali <meerhidayat563@gmail.com>
2022-12-28 22:56:32 +0530

    changedfooter to mobile friendly

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -5,7 +5,7 @@ import four from '../assets/svg/projects/four.svg'
 import five from '../assets/svg/projects/five.svg'
 import six from '../assets/svg/projects/six.svg'
 import seven from '../assets/svg/projects/seven.svg'
-import eight from '../assets/svg/projects/eight.svg'
+
 
 
 export const projectsData = [
