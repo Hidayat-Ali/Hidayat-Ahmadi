7d0267afa1f77ec77311dbf7f5220607c8b990bd
Hidayat-Ali <meerhidayat563@gmail.com>
2023-03-15 20:46:19 +0530

    added some projects

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -3,12 +3,6 @@ export const socialsData = {
     facebook: 'https://www.facebook.com/mir.hidayat.395',
     linkedIn: 'https://www.linkedin.com/in/hidayat-ali-7806271b7/',
     instagram: 'https://www.instagram.com/thisizhidayat/',
-    codepen: 'https://codepen.io/',
     twitter: 'https://twitter.com/HidayatAlimir1',
-    reddit: 'https://www.reddit.com/user/',
-    blogger: 'https://www.blogger.com/',
-    medium: 'https://medium.com/@',
-    stackOverflow: 'https://stackoverflow.com/users/',
-    gitlab: 'https://gitlab.com/',
-    youtube: 'https://youtube.com/'
+   
 }
